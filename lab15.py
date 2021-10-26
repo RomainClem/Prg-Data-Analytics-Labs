6a7e696abb269c35985bfe55c4a8d26acdfb1a5c
Romain Clemencon <romain.clemencon@mycit.ie>
2021-10-26 14:06:14 +0100

    Completed all labs.

--- lab15.py
+++ lab15.py
@@ -1,26 +1,49 @@
 import numpy as np
 
+data = np.genfromtxt('bikeSharing.csv', delimiter=",")
 
-# # Question 2
-# temp = np.copy(data[:,9])*41
-# array[:, 9] *= 41
+# Question 1
+def compareHolidays(data, holiday):
+    subset = data[data[:, 5] == holiday]
+    print ("Number of entries ", len(subset))
+    print ("Mean", np.mean(subset[:, 14]))
 
 
-#Question 3
-# result = data[:, 13] > data[:, 14] # Boolean array 
+
+# compareHolidays(data, 0)
+# compareHolidays(data, 1)
+
+# Question 2
+def normalizedValues(data):
+    temp = np.copy(data[:,9])*41
+    atemp = np.copy(data[:,10])*50
+    hum = np.copy(data[:,11])*100
+    windspeed = np.copy(data[:,12])*67
+    print(temp)
+    print(atemp)
+    print(hum)
+    print(windspeed)
+
+# normalizedValues(data)
+
+# Question 3
+def question3():
+    result = data[:, 13] > data[:, 14] # Boolean array 
+    print ("Percentage of time where causal users > registered", (len(data[result])*100.0)/len(data) )
+
 
 #Question 4
 
 def averagePerCondition(data):
-    conditions = {1:"C", 2:"L", 3:"M", 4:"H"}
+    conditions = {1:"Clear", 2:"Misty", 3:"Light Rain", 4:"Heavy Rain"}
     for key in conditions:
         subesetData = data[data[:,8] == key]
-        print(np.mean(subesetData[:, 15]))
+        print(f"{key}: ", np.mean(subesetData[:, 15]))
 
-data = np.genfromtxt('bikeSharing.csv', delimiter=",")
+
+# averagePerCondition(data)
 
 #Question 5
-
 
 def analyseTemp(data, minvalue, maxvalue):
     hTemp = (data[: ,9]*41)>=minvalue
@@ -31,4 +54,4 @@ def analyseTemp(data, minvalue, maxvalue):
 
 
 for temp in range(1, 40, 5):
-    analyseTemp(data, temp, temp+4)    
+    analyseTemp(data, temp, temp+4)
