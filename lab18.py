2ee1316e518e73f514f98492f759b4f715263df4
Romain Clemencon <romain.clemencon@mycit.ie>
2021-11-09 23:53:56 +0000

    Added function and csv files.

---
+++ lab18.py
@@ -0,0 +1,23 @@
+def question1():
+    print()
+    
+def question2():
+    print()
+    
+def question3():
+    print()
+    
+def question4():
+    print()
+    
+def question5():
+    print()
+    
+def question6():
+    print()
+    
+def question7():
+    print()
+    
+def question8():
+    print()
---
+++ lab19.py
@@ -0,0 +1,17 @@
+def question1():
+    print()
+    
+def question2():
+    print()
+    
+def question3():
+    print()
+    
+def question4():
+    print()
+    
+def question5():
+    print()
+    
+def question6():
+    print()
